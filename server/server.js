d71d698e1ec8bae6cda4ed7b631dc717ddfc58e4
Rohan_Mac <apple@Apples-MacBook-Pro.local>
2018-07-30 16:13:26 +0530

    added get route for any ID

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const http = require('http')
+const { ObjectID } = require('mongodb')
 
 const { Mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
@@ -26,6 +27,23 @@ app.get('/todos', (req, res) => {
   Todo.find().then(result => {
     res.send({result})
   }, err => {
+    res.status(400).send(err)
+  })
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then(todo => {
+    if(!todo){
+      return res.status(404).send()
+    }
+    res.status(200).send(todo)
+  }).catch(err => {
     res.status(400).send(err)
   })
 })
