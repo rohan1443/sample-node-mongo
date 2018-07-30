bfc8eff211d3c8add1214e4d18ba704aec2cfbb5
Rohan_Mac <apple@Apples-MacBook-Pro.local>
2018-07-30 17:28:30 +0530

    added Delete route for an ID

--- server/server.js
+++ server/server.js
@@ -50,6 +50,23 @@ app.get('/todos/:id', (req, res) => {
   })
 })
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndRemove(id).then(todo => {
+    if(!todo){
+      return res.status(404).send()
+    }
+    res.status(200).send(todo)
+  }).catch(err => {
+    res.status(400).send(err)
+  })
+})
+
 app.listen(port, () => {
   console.log(`server started on port ${port}`)
 })
