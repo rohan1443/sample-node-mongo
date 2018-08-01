e5f8d34b66e3ce59dfe6d92f7bb92e8ac420f8eb
Rohan_Mac <apple@Apples-MacBook-Pro.local>
2018-08-01 20:47:02 +0530

    updated with Patch route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const http = require('http')
 const { ObjectID } = require('mongodb')
+const _ = require('lodash')
 
 const { Mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
@@ -30,6 +31,31 @@ app.get('/todos', (req, res) => {
     res.send({result})
   }, err => {
     res.status(400).send(err)
+  })
+})
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id
+  const body = _.pick(req.body, ['text', 'completed'])
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body}, {new: true}).then(todo => {
+    if(!todo){
+      return res.status(404).send()
+    }
+    res.status(200).send(todo)
+  }).catch(e => {
+    res.status(400).send(e)
   })
 })
 
